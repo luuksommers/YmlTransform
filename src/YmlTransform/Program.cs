775c529e3c18e8930849122def5819eeca0a44a0
Luuk Sommers <luuk.sommers@infocaster.net>
2017-10-23 10:34:45 +0200

    Fix multi language field parsing not looping all fields

--- src/YmlTransform/Program.cs
+++ src/YmlTransform/Program.cs
@@ -18,65 +18,7 @@ namespace YmlTransform
             var isValid = CommandLine.Parser.Default.ParseArgumentsStrict(args, options);
             if (isValid)
             {
-                var transformation = Newtonsoft.Json.JsonConvert.DeserializeObject<List<TransformItem>>(File.ReadAllText(options.TransformFile));
-
-                Transform(options.Path, transformation, options.Recursive);
-            }
-        }
-
-        private static void Transform(string path, List<TransformItem> transformations, bool recursive)
-        {
-            var files = Directory.GetFiles(path, "*.yml",
-                recursive ? SearchOption.AllDirectories : SearchOption.TopDirectoryOnly);
-
-            foreach (var file in files)
-            {
-                using (var fs = new FileStream(file, FileMode.OpenOrCreate, FileAccess.ReadWrite, FileShare.None))
-                {
-                    var transformed = false;
-                    var formatter = new YamlSerializationFormatter(null, null);
-                    var item = new ProxyItem(formatter.ReadSerializedItem(fs, Path.GetFileName(file)));
-
-                    foreach (var t in transformations)
-                    {
-                        if (item.Path == t.Path)
-                        {
-                            if (t.Type == "Shared")
-                            {
-                                var field = item.SharedFields.FirstOrDefault(a => a.FieldId == t.FieldId) as ProxyFieldValue;
-                                if (field != null)
-                                {
-                                    Console.WriteLine($"Updating file {file} section {t.Type} id {t.FieldId} to {t.Value}");
-                                    field.Value = t.Value;
-                                    transformed = true;
-                                }
-                            }
-                            else if (t.Type == "Languages")
-                            {
-                                var field = item.Versions
-                                    .Where(a => t.Languages == "*")
-                                    .SelectMany(a => a.Fields)
-                                    .FirstOrDefault(a => a.FieldId == t.FieldId) as ProxyFieldValue;
-                                if (field != null)
-                                {
-                                    Console.WriteLine($"Updating file {file} section {t.Type} id {t.FieldId} to {t.Value}");
-                                    field.Value = t.Value;
-                                    transformed = true;
-                                }
-                            }
-                        }
-                    }
-
-
-
-                    if (transformed)
-                    {
-                        Console.WriteLine($"Transformed: {file}");
-                        fs.SetLength(0);
-                        formatter.WriteSerializedItem(item, fs);
-                    }
-                }
-
+                YmlTransformer.TransformPath(options.Path, options.TransformFile, options.Recursive);
             }
         }
 
@@ -90,16 +32,6 @@ namespace YmlTransform
 
             [Option('t', "transform", Required = true, HelpText = "Transformation file")]
             public string TransformFile { get; set; }
-        }
-
-        class TransformItem
-        {
-            public string Path { get; set; }
-            public string Type { get; set; }
-            public string Languages { get; set; }
-            public Guid FieldId { get; set; }
-            public string Value { get; set; }
-            public string Hint { get; set; }
         }
     }
 }
---
+++ src/YmlTransform/TransformItem.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace YmlTransform
+{
+    class TransformItem
+    {
+        public string Path { get; set; }
+        public string Type { get; set; }
+        public string Languages { get; set; }
+        public Guid FieldId { get; set; }
+        public string Value { get; set; }
+        public string Hint { get; set; }
+    }
+}
